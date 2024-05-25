43a0980969dca50b5d92f0e7d8c1d15404275351
Praveen Ranjan <115415907+Praveen1800@users.noreply.github.com>
2024-05-25 15:14:14 +0530

    Add files via upload

---
+++ And.java
@@ -0,0 +1,12 @@
+public class And{
+    public static void main(String[] args) {
+        int g = 60;
+        if(g>20 && g<50){
+            System.out.println("i am bihari");
+        }
+        else{
+            System.out.println("i am not bihari");
+        }
+    }
+    
+}
